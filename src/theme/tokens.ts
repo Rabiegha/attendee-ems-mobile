379b1d5ac8c39979598242a6b08960c84adb903d
rabie Gharghar <80117176+Rabiegha@users.noreply.github.com>
2025-10-28 19:08:00 +0100

    fix: use React Native valid fontWeight values

--- src/theme/tokens.ts
+++ src/theme/tokens.ts
@@ -72,11 +72,11 @@ export const fontSize = {
 };
 
 export const fontWeight = {
-  normal: '400',
-  medium: '500',
-  semibold: '600',
-  bold: '700',
-} as const;
+  normal: 'normal' as const,
+  medium: '500' as const,
+  semibold: '600' as const,
+  bold: 'bold' as const,
+};
 
 export const shadows = {
   sm: {
