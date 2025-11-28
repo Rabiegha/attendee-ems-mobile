a823e5fc39c1de4c2d54d338c21bfd7a3a987e20
RabyGha <80117176+Rabiegha@users.noreply.github.com>
2025-11-28 14:06:32 +0100

    test

--- src/config/printNodeApi.ts
+++ src/config/printNodeApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { PRINT_NODE_URL } from './config';
 import { Buffer } from 'buffer';
 
-const apiKey = 'PrDdPPH6bJJpWSYwONmfapRUCCkL-770o5ZTsWSyY7g';
+const apiKey = process.env.EXPO_PUBLIC_PRINTNODE_API_KEY || '';
 const base64ApiKey = Buffer.from(`${apiKey}:`).toString('base64');
 
 const printNodeApi = axios.create({
